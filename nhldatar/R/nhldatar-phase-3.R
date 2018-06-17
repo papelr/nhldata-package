f4a3ea747ea8ff2b89491fa615dec8c539bd6d1b
papelr <robertpapel@gmail.com>
2018-06-16 20:12:34 -0500

    basically don't know where to start with prediction models lol

--- nhldatar/R/nhldatar-phase-3.R
+++ nhldatar/R/nhldatar-phase-3.R
@@ -1,6 +1,53 @@
 #' ---------
 #' Title: Phase 3, for nhldatar package
-#' Subtitle: 
+#' Subtitle: Am I gonna build a stack? 
 #' Author: Robert Papel
 #' ---------
+
+#'###### -------------**Libraries**---------------------- ######
+
+library(stringr)
+library(tidyverse)
+library(LiblineaR)
+library(mltools)
+library(class)
+library(glmnet)
+library(data.table)
+
+#'###### -------------**Removing Asterisks**---------------------- ######
+
+outcomes$Home <- gsub("\\*", "", outcomes$Home)
+outcomes$Visitor <- gsub("\\*", "", outcomes$Visitor)
+
+#'###### -------------**Average Goals**---------------------- ######
+
+outcomes %>% 
+  summarise(
+    avg_home_goals = mean(outcomes$GHome, na.rm = TRUE),
+    avg_visitor_goals = mean(outcomes$GVisitor, na.rm = TRUE),
+    total_avg_goals = ((avg_home_goals + avg_visitor_goals) / 2)
+  ) -> goals
+
+# Home team goal average: 3.014
+# Away team goal average: 2.711
+# Average total goals: 2.863
+
+#'###### -------------**TBD Model**---------------------- ######
+
+outcomes <- outcomes %>% 
+  mutate(
+    avg_home_goals = mean(outcomes$GHome, na.rm = TRUE),
+    avg_visitor_goals = mean(outcomes$GVisitor, na.rm = TRUE),
+    total_avg_goals = ((avg_home_goals + avg_visitor_goals) / 2)
+  ) 
+
+
+
+
+
+
+
+
+
+
 
