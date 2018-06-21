e76cc702f25054e04c4eddb4fbe2d9931d27e31b
papelr <robertpapel@gmail.com>
2018-06-20 21:58:45 -0500

    created a "Win" category, based on if Home team won or not

--- nhldatar/R/nhldatar-phase-3.R
+++ nhldatar/R/nhldatar-phase-3.R
@@ -1,6 +1,6 @@
 #' ---------
 #' Title: Phase 3, for nhldatar package
-#' Subtitle: Adapted from David Sheehan's Poisson example 
+#' Subtitle: Models 
 #' Author: Robert Papel
 #' ---------
 
@@ -8,11 +8,12 @@
 
 library(stringr)
 library(tidyverse)
-library(LiblineaR)
-library(mltools)
-library(class)
 library(glmnet)
 library(data.table)
+library(e1071)
+library(klaR)
+library(bnlearn)
+library(rpart)
 
 #'###### -------------**Removing Asterisks**---------------------- ######
 
@@ -39,6 +40,7 @@ out_head <- as.data.frame(out_head)
 
 #'###### -------------**Poisson Distribtion Model**-------------- ######
 
+# Adapted from David Sheehan's Poisson example (lol as if it works)
 outcomes <- outcomes %>% 
   mutate(
     avg_home_goals = mean(outcomes$GHome, na.rm = TRUE),
@@ -91,12 +93,83 @@ simulate_game <- function(stat_model, homeTeam, awayTeam, max_goals = 10) {
                                        opponent = homeTeam), 
                             type ="response")
   
-  dpois(0: max_goals, home_goals) %>%  
+  dpois(0: max_goals, home_goals) %o%  
     dpois(0: max_goals, away_goals) 
 }
 
 simulate_game(model_one, "Nashville Predators", "Chicago Blackhawks", 
           max_goals = 10)
+
+
+
+
+#'###### -------------**Data set split**-------------- ######
+
+# Takes off the "Date" column
+outcomes <- outcomes[,-1]  
+
+# Splits data set, but not randomly distributed
+# train <- outcomes[1:24934, ]
+# test <- outcomes[24935:31168, ]
+
+# Adds "Train" column, with 1 or 0
+outcomes$"Train" <- if_else(runif(nrow(outcomes)) < 0.80, 1 ,0)
+train_column_number <- grep("Train", names(outcomes))
+
+outcomes <- mutate(outcomes, Win = GHome > GVisitor)
+
+outcomes_train <- outcomes[outcomes$Train == 1, -train_column_number]
+outcomes_test <- outcomes[outcomes$Train == 0, -train_column_number]
+
+outcomes_train$Home <- as.factor(outcomes_train$Home)
+outcomes_train$Visitor <- as.factor(outcomes_train$Visitor)
+
+
+
+
+#'###### -------------**Naive Bayes Model**-------------- ######
+
+# Column number of train / test indicators 
+train_column_number <- grep("Train", names(outcomes))
+
+
+
+# # Separate Train / Test
+# train_outcomes <- outcomes[outcomes$Train == 1, -train_column_number]
+# test_outcomes <- outcomes[outcomes$Train == 0, -train_column_number]
+
+# Naive Bayes model (known as model two)
+model_two <- naiveBayes(outcomes$Home ~  outcomes$Visitor,
+                        data = train_outcomes)
+summary(model_two)
+str(model_two)
+
+# Prediction
+test_predict_outcomes <- predict(model_two, test_outcomes[,-1])
+
+# Confusion Matrix
+table(predict = test_predict_outcomes, test_outcomes$Home)
+
+
+#'###### -------------**Decision Treeeeees**-------------- ######
+
+# Training set
+# tree_train <- cbind(x_train, y_train)
+
+# Grow tree 
+model_three <- rpart(outcomes_train$Home ~ ., data = outcomes_train)
+summary(fit)
+
+# Predict Output 
+predicted = predict(model_three, x_test)
+
+
+
+
+#'###### -------------**Detritus**-------------- ######
+
+outcomes$"Win" <- gsub(0, "No", outcomes$Train)
+outcomes$"Win" <- gsub(1, "Yes", outcomes$Train)
 
 
 
