9d4e4f6465c64fc7d3f8b5475a0778e7cf1ad128
papelr <robertpapel@gmail.com>
2018-06-24 17:26:09 -0500

    a glm model on Win (T/F) but does this actually work who the hell knows

--- nhldatar/R/nhldatar-phase-3.R
+++ nhldatar/R/nhldatar-phase-3.R
@@ -1,175 +1,53 @@
 #' ---------
-#' Title: Phase 3, for nhldatar package
-#' Subtitle: Models 
+#' Title: Linear Model 
+#' Subtitle: lm()
 #' Author: Robert Papel
 #' ---------
 
 #'###### -------------**Libraries**---------------------- ######
 
-library(stringr)
 library(tidyverse)
-library(glmnet)
-library(data.table)
-library(e1071)
-library(klaR)
-library(bnlearn)
-library(rpart)
+library(vtreat)
 
-#'###### -------------**Removing Asterisks**---------------------- ######
+#'###### -------------**Linear Model**---------------------- ######
 
-outcomes$Home <- gsub("\\*", "", outcomes$Home)
-outcomes$Visitor <- gsub("\\*", "", outcomes$Visitor)
+# Gets rid of years with Ties (so, 1990-2006)
+outcomes <- outcomes[-c(1:16632), ]
 
-#'###### -------------**Average Goals**---------------------- ######
+# Change "Date" column format in outcomes
+outcomes$Date <- sub("(\\d{4})-.*", "\\1", outcomes$Date)
 
-outcomes %>% 
-  summarise(
-    avg_home_goals = mean(outcomes$GHome, na.rm = TRUE),
-    avg_visitor_goals = mean(outcomes$GVisitor, na.rm = TRUE),
-    total_avg_goals = ((avg_home_goals + avg_visitor_goals) / 2)
-  ) -> goals
-
-# Home team goal average: 3.014
-# Away team goal average: 2.711
-# Average total goals: 2.863
-
-#'###### -------------**Dput for CV Question**-------------- ######
-
-out_head <- dput(head(outcomes, 1000))
-out_head <- as.data.frame(out_head)
-
-#'###### -------------**Poisson Distribtion Model**-------------- ######
-
-# Adapted from David Sheehan's Poisson example (lol as if it works)
-outcomes <- outcomes %>% 
-  mutate(
-    avg_home_goals = mean(outcomes$GHome, na.rm = TRUE),
-    avg_visitor_goals = mean(outcomes$GVisitor, na.rm = TRUE),
-    total_avg_goals = ((avg_home_goals + avg_visitor_goals) / 2)
-  ) 
-
-# Using number of goals for prediction model
-model_one <- 
-  rbind(
-    data.frame(goals = outcomes$GHome,
-               team = outcomes$Home,
-               opponent = outcomes$Visitor,
-               home = 1),
-    data.frame(goals = outcomes$GVisitor,
-               team = outcomes$Visitor,
-               opponent = outcomes$Home,
-               home = 0)) %>%
-  glm(goals ~ home + team + opponent, family = poisson (link = log), data = .)
-summary(model_one)
-
-# Prediction for NSH number of goals
-predict(model_one,
-        data.frame(
-          home = 1, 
-          team = "Nashville Predators", 
-          opponent = "Chicago Blackhawks"), 
-        type ="response")
-
-# Prediction for CHI number of goals
-predict(model_one,
-        data.frame(
-          home = 0, 
-          team = "Chicago Blackhawks", 
-          opponent = "Nashville Predators"), 
-        type ="response")
-
-# Probability function / matrix
-simulate_game <- function(stat_model, homeTeam, awayTeam, max_goals = 10) {
-  
-  home_goals <- predict(model_one,
-                            data.frame(home = 1, 
-                                       team = homeTeam,
-                                       opponent = awayTeam), 
-                            type ="response")
-  
-  away_goals <- predict(model_one, 
-                            data.frame(home = 0, 
-                                       team = awayTeam, 
-                                       opponent = homeTeam), 
-                            type ="response")
-  
-  dpois(0: max_goals, home_goals) %o%  
-    dpois(0: max_goals, away_goals) 
-}
-
-simulate_game(model_one, "Nashville Predators", "Chicago Blackhawks", 
-          max_goals = 10)
-
-
-
-
-#'###### -------------**Data set split**-------------- ######
-
-# Takes off the "Date" column
-outcomes <- outcomes[,-1]  
-
-# Splits data set, but not randomly distributed
-# train <- outcomes[1:24934, ]
-# test <- outcomes[24935:31168, ]
-
-# Adds "Train" column, with 1 or 0
-outcomes$"Train" <- if_else(runif(nrow(outcomes)) < 0.80, 1 ,0)
-train_column_number <- grep("Train", names(outcomes))
-
+# Adds "Win" column, if home team won
 outcomes <- mutate(outcomes, Win = GHome > GVisitor)
+outcomes$Year <- outcomes$Date
+outcomes$Year <- as.numeric(outcomes$Year)
+outcomes <- outcomes[, -1]
 
-outcomes_train <- outcomes[outcomes$Train == 1, -train_column_number]
-outcomes_test <- outcomes[outcomes$Train == 0, -train_column_number]
+# Get percentage of home team wins in outcomes set
+summary(outcomes) # 7941 TRUEs
+home_win_perc <- 7941 / 14532 # Home team wins 54.6% of the time
+away_win_perc <- 6591 / 14532
 
-outcomes_train$Home <- as.factor(outcomes_train$Home)
-outcomes_train$Visitor <- as.factor(outcomes_train$Visitor)
+# Linear model on full data set
+lm_model_full <- lm(Win ~ GHome + GVisitor, outcomes) # win = TRUE when home wins
+summary(lm_model_full)
+res_full <- lm_model$residuals
+rmse_full <- sqrt(mean(res^2))
 
+# Splitting the data
+N <- nrow(outcomes)
+target <- round(N * 0.75)
+gp <- runif(N)
+outcomes_train <- outcomes[gp < 0.75, ]
+outcomes_test <- outcomes[gp >= 0.75, ]
 
+# Linear model on training set
+lm_model_train <- glm(Win ~ GHome + GVisitor, outcomes_train, family = "poisson")
+summary(lm_model_train)
+res_train <- lm_model_train$residuals
+rmse_train <- sqrt(mean(res_train^2))
 
-
-#'###### -------------**Naive Bayes Model**-------------- ######
-
-# Column number of train / test indicators 
-train_column_number <- grep("Train", names(outcomes))
-
-
-
-# # Separate Train / Test
-# train_outcomes <- outcomes[outcomes$Train == 1, -train_column_number]
-# test_outcomes <- outcomes[outcomes$Train == 0, -train_column_number]
-
-# Naive Bayes model (known as model two)
-model_two <- naiveBayes(outcomes$Home ~  outcomes$Visitor,
-                        data = train_outcomes)
-summary(model_two)
-str(model_two)
-
-# Prediction
-test_predict_outcomes <- predict(model_two, test_outcomes[,-1])
-
-# Confusion Matrix
-table(predict = test_predict_outcomes, test_outcomes$Home)
-
-
-#'###### -------------**Decision Treeeeees**-------------- ######
-
-# Training set
-# tree_train <- cbind(x_train, y_train)
-
-# Grow tree 
-model_three <- rpart(outcomes_train$Home ~ ., data = outcomes_train)
-summary(fit)
-
-# Predict Output 
-predicted = predict(model_three, x_test)
-
-
-
-
-#'###### -------------**Detritus**-------------- ######
-
-outcomes$"Win" <- gsub(0, "No", outcomes$Train)
-outcomes$"Win" <- gsub(1, "Yes", outcomes$Train)
-
-
+# Predict using split sets
+outcomes_train$Prediction <- predict(lm_model_train)
+outcomes_test$Prediction <- predict(lm_model_train, outcomes_test, type = "response")
 
