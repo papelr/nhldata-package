4c8e73e4061db33a2cf9a884afe189e09ce9ef5e
papelr <robertpapel@gmail.com>
2018-06-26 10:48:06 -0500

    caret lets gooooo

--- nhldatar/R/nhldatar-phase-6-caret.R
+++ nhldatar/R/nhldatar-phase-6-caret.R
@@ -17,18 +17,29 @@ outcomes$Date <- as.numeric(outcomes$Date)
 outcomes$Att. <- as.numeric(gsub("," , "", outcomes$Att.))
 outcomes$Visitor <- as.factor(outcomes$Visitor)
 outcomes$Home <- as.factor(outcomes$Home)
-outcomes$GVisitor <- as.numeric(outcomes$GVisitor)
-outcomes$GHome <- as.numeric(outcomes$GHome)
+outcomes$GVisitor <- as.factor(outcomes$GVisitor)
+outcomes$GHome <- as.factor(outcomes$GHome)
+outcomes <- outcomes[, -6]
 
 #'###### -------------**Caret Package**---------------------- ######
+
+myControl <- trainControl(
+  method = "cv", number = 10,
+  summaryFunction = twoClassSummary,
+  classProbs = TRUE, # IMPORTANT!
+  verboseIter = TRUE
+)
 
 caret_m1 <- train(
   GHome ~ GVisitor + Home + Visitor, outcomes,
   method = "glmnet",
-  tuneGrid = expand.grid(alpha = 0:1,
-              lambda = seq(0.0001, 1, length = 100)),
-  trControl = trainControl(
-    method = "cv", number = 10,
-    verboseIter = TRUE
+  na.action = na.pass,
+  preProcess = "medianImpute",
+  trControl = myControl
   )
 )
+
+max(model[["results"]][["ROC"]])
+
+tuneGrid = expand.grid(alpha = 0:1,
+                       lambda = seq(0.0001, 1, length = 100))
