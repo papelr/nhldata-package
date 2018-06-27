c64d0d8ab0fcfaa308da50d00448d05c42a4b62e
papelr <robertpapel@gmail.com>
2018-06-27 14:20:58 -0500

    caret / probably wont need bivpois

---
+++ nhldatar-phase-6-caret.R
@@ -0,0 +1,13 @@
+#' ---------
+#' Title: Linear Model 
+#' Subtitle: caret package models
+#' Author: Robert Papel
+#' ---------
+
+#'###### -------------**Libraries**---------------------- ######
+
+library(tidyverse)
+library(caret)
+
+#'###### -------------**Caret Package**---------------------- ######
+
