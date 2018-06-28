df80a3dc6f24179277b298a2f7a73ae912a0a574
papelr <robertpapel@gmail.com>
2018-06-28 10:22:00 -0500

    VAR forecasts work!!!!

--- nhldatar/R/nhldatar-NSH-forecast.R
+++ nhldatar/R/nhldatar-NSH-forecast.R
@@ -10,7 +10,7 @@ library(tidyverse)
 library(rvest)
 library(vars)
 library(forecast)
-library(prophet)
+library(imputeTS)
 
 #'###### -------------**Links for NSH**---------- ######
 
@@ -59,13 +59,55 @@ NSH_stats <- NSH_stats %>%
 # Creating time series
 NSH_ts <- ts(NSH_stats[ ,setdiff(names(NSH_stats), c("Team", "Year"))],
             start = c(2006),
-            end = c(2017),
+            end = c(2018),
             frequency = 1)
 class(NSH_ts)
+NSH_ts <- na.mean(NSH_ts, option = "mean")
 
 # Autoplot
-autoplot(NSH_ts[,3]) 
+autoplot(NSH_ts, facets = T) +
+  scale_x_continuous(breaks = seq(2006,2018))
+Acf(NSH_ts[,6])
 
-#'###### -------------**Forecasting on Team Year**-------------- ######
+#'###### -------------**Forecasting on Team Seasons**-------------- ######
 
-# ARIMA models
+# ARIMA model with GF, mainly White Noise
+NSH.m1 <- auto.arima(NSH_ts[, "S"])
+checkresiduals(NSH.m1)
+
+forecast.m1 <- forecast(NSH.m1, h = 2) 
+autoplot(forecast.m1) 
+# scale_x_continuous(breaks = seq(2006, 2022))
+
+# ARIMA model with GF, GA, S, also mainly White Noise
+xreg.m2 <- data.frame(GF = NSH_ts[, "GF"],
+              GA = NSH_ts[, "GA"],
+              S = NSH_ts[, "S"])
+NSH.m2 <- auto.arima(NSH_ts[, "PTS"],
+                     xreg = (xreg.m2),
+                     stationary = T)
+checkresiduals(NSH.m2)
+
+forecast.m2 <- forecast(NSH.m2, xreg = rep(5, 1))
+autoplot(forecast.m2) +
+  scale_x_continuous(breaks = seq(2006, 2022)) +
+  ylim(65, 125)
+
+
+# VARs models
+var1 <- VAR(NSH_ts[, 3:5], p = 1, type = "both", ic = c("AIC"))
+forecast.var1 <- forecast(var1, h = 2) 
+autoplot(forecast.var1) +
+  scale_x_continuous(breaks = seq(2006, 2022))
+
+var2 <- VAR(NSH_ts[, 3:4], p = 2)
+var2 %>% 
+  forecast() %>% 
+  autoplot()
+
+var3 <- VAR(NSH_ts[, 3:4], p = 3)
+var3 %>% 
+  forecast() %>% 
+  autoplot()
+
+
